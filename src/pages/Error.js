c6048790a536bc2c55235bd5996052346ab548d9
Efing Bv <efingbv@Efingbv2-0>
2023-01-06 20:02:17 +0100

    error handler + page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "../style/Error.css"
 
 const Error = () => {
-  return <div>Error page</div>;
+  return (
+    <div className="error">
+      <div className="error_content">
+        <p className="error_number">404</p>
+        <p className="error_text">
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+      </div>
+      <Link to="/" className="error_link">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  );
 };
 
 export default Error;
