66f2b93d36537618fe237815a3c70b148b9fcd98
Champagne Dylan <efingbv@gmail.com>
2023-01-07 00:32:23 +0100

    fixed navbar css + finished About pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,44 @@
 import React from "react";
 
 import Section from "../components/Banner";
+import DropDown from "../components/dropDown";
 
+import "../style/About.css";
 import banner from "../assets/aboutBanner.webp"
 
 const About = () => {
+
+  const aboutDropDown = [
+    {
+      title: "Fiabilité",
+      content: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+    },
+    {
+      title: "Respect",
+      content: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+    },
+    {
+      title: "Service",
+      content: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+    },
+    {
+      title: "Sécurité",
+      content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+    }
+  ]
+
   return (
     <React.Fragment>
-      <Section src={banner} />
+      <div>
+        <Section src={banner} />
+        <div className="aboutContainer">
+          {aboutDropDown.map((drop) => {
+            return (
+              <DropDown title={drop.title} content={drop.content} />
+            )
+          })}
+        </div>
+      </div>
     </React.Fragment>
   );
 };
